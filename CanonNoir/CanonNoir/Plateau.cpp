7fd6405fda64ae58f086d7a7af2b8e48d513a8cc
nicolas.desfeux <nicolas.desfeux@cda8d8e1-4e2b-cab8-d90d-f9c12049dff8>
2010-12-08 22:51:55 +0000

    Ca marche pas et c'est chaint !!!!!!!

--- CanonNoir/CanonNoir/Plateau.cpp
+++ CanonNoir/CanonNoir/Plateau.cpp
@@ -1,19 +1,19 @@
 #include "Plateau.h"
 
-Plateau::Plateau(int lo, int la){
+Plateau::Plateau(){
 			for(int i=0;i<11;i++){
-				for(int i=0;i<11;i++){
-					
+				for(int j=0;j<8;j++){
+					tab_case[i][j]=Case();
 				}
 			}
 		}
 
 	void Plateau::libereCase(int x,int y){
-		tab_case[x][y].libereCase();
+		Case test = tab_case[x][y];
 	}
 
-	Etat& Plateau::getEtat(int x, int y){
-		return tab_case[x][y].getEtat();
+	Etat Plateau::getEtat(int x, int y){
+		return tab_case[x][y].Case::getEtat();
 	}
 
 	bool Plateau::estNavigable(int x, int y){
